d18b8bbf790fe4e4cfcb41327ef02e7cc9148bfd
muzerow <muzerow6@gmail.com>
2022-09-22 17:48:28 +0300

    Add overwrite parameter to mt_download.r

--- R/mt_download.r
+++ R/mt_download.r
@@ -8,7 +8,20 @@
 #'
 #' @export
 
-mt_raw_download <- function(download_link, file_path) {
-  download.file(download_link, file_path)
-  gunzip(file_path)
+mt_raw_download <- function(download_link, file_path, overwrite = F) {
+  if (endsWith(file_path, ".csv")) {
+    gz_file_path <- paste0(file_path, ".gz")
+  } else if (endsWith(file_path, ".gz")) {
+    gz_file_path <- file_path
+  } else {
+    stop("Incorrect file_path argument - .csv or .gz file format required")
+  }
+
+  download.file(download_link, gz_file_path)
+
+  if (isTRUE(overwrite)) {
+    file.remove(file_path)
+  }
+
+  gunzip(gz_file_path)
 }
